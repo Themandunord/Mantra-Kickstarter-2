8b521aed60c298682d799c13bcd562ee793920c0
Lespagnol Rémy <remy.lespagnol@gmail.com>
2017-08-23 22:09:05 +0200

    Remove .idea folder from gitignore

--- client/configs/context.js
+++ client/configs/context.js
@@ -1,15 +1,15 @@
 import * as Collections from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
-import {FlowRouter} from 'meteor/kadira:flow-router';
 import {ReactiveDict} from 'meteor/reactive-dict';
 import {Tracker} from 'meteor/tracker';
+import {Accounts} from 'meteor/accounts-base';
 
 export default function () {
-  return {
-    Meteor,
-    FlowRouter,
-    Collections,
-    LocalState: new ReactiveDict(),
-    Tracker
-  };
+    return {
+        Meteor,
+        Collections,
+        LocalState: new ReactiveDict(),
+        Tracker,
+        Accounts,
+    };
 }
--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {mount} from 'react-mounter';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
+/* Layouts */
 import MainLayout from '/client/modules/core/components/main_layout.jsx';
+
+/* Containers */
 import Home from '/client/modules/core/components/home.jsx';
 
-export default function (injectDeps, {FlowRouter}) {
-  const MainLayoutCtx = injectDeps(MainLayout);
 
-  FlowRouter.route('/', {
-    name: 'home',
-    action() {
-      mount(MainLayoutCtx, {
-        content: () => (<Home />)
-      });
+export default function (injectDeps, {LocalState}) {
+    const MainLayoutCtx = injectDeps(MainLayout);
+
+    ReactDOM.render(
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={Home}/>
+            </div>
+        </BrowserRouter>
+        ,
+        getRootNode('root')
+    );
+}
+
+function getRootNode(rootId) {
+    const rootNode = document.getElementById(rootId);
+
+    if (rootNode) {
+        return rootNode;
     }
-  });
+
+    const rootNodeHtml = '<div id="' + rootId + '"></div>';
+    const body = document.getElementsByTagName('body')[0];
+    body.insertAdjacentHTML('beforeend', rootNodeHtml);
+
+    return document.getElementById(rootId);
 }
