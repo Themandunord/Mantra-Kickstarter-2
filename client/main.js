eaeba25d4026517606ea5b617c58600e53241683
Lespagnol Rémy <remy.lespagnol@gmail.com>
2017-08-24 19:13:40 +0200

    Inject an inital loading

--- client/main.js
+++ client/main.js
@@ -22,3 +22,13 @@ const app = createApp(context);
 // load modules
 app.loadModule(coreModule);
 app.init();
+
+
+// Remove the loading spinner when the user have loaded all the scripts
+if (Meteor.isClient) {
+    Meteor.startup(function() {
+        setTimeout(function() {
+            $("#inject-loader-wrapper").fadeOut(200, function() { $(this).remove(); });
+        }, 500);
+    });
+}
--- server/main.js
+++ server/main.js
@@ -6,3 +6,8 @@ _ = lodash;
 
 publications();
 methods();
+
+// Inject Loader's html before script was loaded
+if (Meteor.isServer) {
+    Inject.rawHead("loader", Assets.getText('loader/loader.html'));
+}
