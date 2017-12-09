4d5c7258150c0127ece326520fd84f32db33b751
Lespagnol Rémy <remy.lespagnol@gmail.com>
2017-12-09 16:29:04 +0100

    Update the layout with antd demo

--- client/modules/core/components/layout.jsx
+++ client/modules/core/components/layout.jsx
@@ -2,60 +2,83 @@ import React from 'react';
 import AntLayout from 'antd/lib/layout';
 import Menu from 'antd/lib/menu';
 import Icon from 'antd/lib/icon';
-const { Header, Content, Footer, Sider } = AntLayout;
+
+
+const {Header, Content, Footer, Sider} = AntLayout;
+const {SubMenu} = Menu;
 
 class Layout extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      collapsed: false
+        this.state = {
+            collapsed: true
+        }
     }
-  }
 
-  render() {
-    return (
-        <AntLayout>
-          <Sider
-              breakpoint="lg"
-              collapsible
-              collapsed={this.state.collapsed}
-              onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
-          >
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-              <Menu.Item key="1">
-                <Icon type="user" />
-                <span className="nav-text">nav 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="video-camera" />
-                <span className="nav-text">nav 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="upload" />
-                <span className="nav-text">nav 3</span>
-              </Menu.Item>
-              <Menu.Item key="4">
-                <Icon type="user" />
-                <span className="nav-text">nav 4</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <AntLayout>
-            <Header style={{ background: '#fff', padding: 0 }} />
-            <Content style={{ margin: '24px 16px 0' }}>
-              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                {this.props.children}
-              </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>
-              Ant Design ©2016 Created by Ant UED
-            </Footer>
-          </AntLayout>
-        </AntLayout>
-    );
-  }
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
+    render() {
+        return (
+            <AntLayout>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                >
+                    <div className="logo"/>
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                        <Menu.Item key="1">
+                            <Icon type="pie-chart"/>
+                            <span>Option 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="desktop"/>
+                            <span>Option 2</span>
+                        </Menu.Item>
+                        <SubMenu
+                            key="sub1"
+                            title={<span><Icon type="user"/><span>User</span></span>}
+                        >
+                            <Menu.Item key="3">Tom</Menu.Item>
+                            <Menu.Item key="4">Bill</Menu.Item>
+                            <Menu.Item key="5">Alex</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub2"
+                            title={<span><Icon type="team"/><span>Team</span></span>}
+                        >
+                            <Menu.Item key="6">Team 1</Menu.Item>
+                            <Menu.Item key="8">Team 2</Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="9">
+                            <Icon type="file"/>
+                            <span>File</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <AntLayout>
+                    <Header style={{background: '#fff', padding: 0}}>
+                        <Icon
+                            className="trigger"
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                    <Content style={{margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280}}>
+                        {this.props.children}
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>
+                        Ant Design ©2016 Created by Ant UED
+                    </Footer>
+                </AntLayout>
+            </AntLayout>
+        );
+    }
 }
 
 export default Layout;
