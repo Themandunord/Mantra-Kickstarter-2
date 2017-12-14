f6e12b98fae8194afda5fc7bc8c1b63625698fd0
Lespagnol Rémy <remy.lespagnol@gmail.com>
2017-12-14 17:31:29 +0100

    Stable version with 1.6.1-beta.18

--- client/modules/core/actions/tests/layout.js
+++
@@ -1,8 +0,0 @@
-const {describe, it} = global;
-import {expect} from 'chai';
-import {spy, stub} from 'sinon';
-import actions from '../layout';
-
-describe('core.actions.layout', () => {
-  it('should do something');
-});
--- client/modules/layout/components/tests/layout.js
+++
@@ -1,8 +0,0 @@
-const {describe, it} = global;
-import {expect} from 'chai';
-import {shallow} from 'enzyme';
-import Layout from '../layout';
-
-describe('core.components.layout', () => {
-  it('should do something');
-});
