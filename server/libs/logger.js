df42163d92e807211c037450ec9b5ec861629b4a
Lespagnol Rémy <remy.lespagnol@gmail.com>
2017-12-15 22:43:42 +0100

    Configure chalk and create a logger class

---
+++ server/libs/logger.js
@@ -0,0 +1,20 @@
+/* eslint no-console: 0 */
+import chalk from 'chalk';
+
+export default class Logger {
+  static info(message) {
+    console.log(chalk.blue(message));
+  }
+
+  static success(message) {
+    console.log(chalk.green(message));
+  }
+
+  static warning(message) {
+    console.log(chalk.yellow(message));
+  }
+
+  static error(message) {
+    console.log(chalk.red(message));
+  }
+}
