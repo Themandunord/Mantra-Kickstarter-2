3b6b44fdab55918901fed348a74de6a22753ac72
Lespagnol Rémy <remy.lespagnol@gmail.com>
2017-12-16 11:46:50 +0100

    add chalk config to main.js

---
+++ server/configs/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+export default function(){
+  chalk();
+}
--- server/main.js
+++ server/main.js
@@ -1,11 +1,21 @@
 import publications from './publications';
 import methods from './methods';
+import configs from './configs';
+import Logger from './libs/logger';
 
 // Replace Undescore with lodash
 _ = lodash;
 
+Logger.info('Load configs');
+configs();
+Logger.info('Load publication');
 publications();
+Logger.info('Load methods');
 methods();
+
+Logger.success('Server is started');
+Logger.warning('Server is started');
+Logger.error('Server is started');
 
 // Inject Loader's html before script was loaded
 if (Meteor.isServer) {
