ff0b327c60f9c4de6879d545fa00616e965607af
Lespagnol Rémy <remy.lespagnol@gmail.com>
2017-12-16 13:34:54 +0100

    Inject initial data

--- server/configs/index.js
+++ server/configs/index.js
@@ -1,9 +1,11 @@
 import browserPolicy from './browser_policy';
 import email from './email';
 import mongo from './mongo-indexes';
+import initialData from './initial-data';
 
 export default function () {
   browserPolicy();
   email();
   mongo();
+  initialData();
 }
