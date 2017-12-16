404b60159d522e74d8087c8260757909b611faa6
Lespagnol Rémy <remy.lespagnol@gmail.com>
2017-12-16 17:03:21 +0100

    refacto sider menu with a logo and a custom component

--- client/modules/layout/components/basic_layout.jsx
+++ client/modules/layout/components/basic_layout.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import autoBind from 'react-autobind';
 import Layout from 'antd/lib/layout';
-import Menu from 'antd/lib/menu';
-import Icon from 'antd/lib/icon';
+import SiderMenu from '/client/modules/core/components/sider_menu';
 import GlobalHeader from '/client/modules/core/containers/global_header';
 
-const {Content, Sider} = Layout;
-const {SubMenu} = Menu;
+const {Content} = Layout;
 
 class BasicLayout extends React.Component {
     constructor(props) {
@@ -28,43 +26,8 @@ class BasicLayout extends React.Component {
     render() {
         return (
             <Layout>
-                <Sider
-                    trigger={null}
-                    collapsible
-                    collapsed={this.state.collapsed}
-                >
-                    <div className="logo"/>
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                        <Menu.Item key="1">
-                            <Icon type="pie-chart"/>
-                            <span>Option 1</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="desktop"/>
-                            <span>Option 2</span>
-                        </Menu.Item>
-                        <SubMenu
-                            key="sub1"
-                            title={<span><Icon type="user"/><span>User</span></span>}
-                        >
-                            <Menu.Item key="3">Tom</Menu.Item>
-                            <Menu.Item key="4">Bill</Menu.Item>
-                            <Menu.Item key="5">Alex</Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="sub2"
-                            title={<span><Icon type="team"/><span>Team</span></span>}
-                        >
-                            <Menu.Item key="6">Team 1</Menu.Item>
-                            <Menu.Item key="8">Team 2</Menu.Item>
-                        </SubMenu>
-                        <Menu.Item key="9">
-                            <Icon type="file"/>
-                            <span>File</span>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-
+                <SiderMenu
+                    collapsed={this.state.collapsed}/>
                 <Layout>
                     <GlobalHeader
                         collapsed={this.state.collapsed}
